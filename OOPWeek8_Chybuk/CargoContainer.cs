0b29b024940d6897af9c76bbab54e23332057495
wa1d7 <ignat.chybuk@gmail.com>
2026-03-24 19:15:10 +0200

    CargoContainer class added + class realisation. 2 mins for program cs and done

---
+++ OOPWeek8_Chybuk/CargoContainer.cs
@@ -0,0 +1,19 @@
+﻿namespace OOPWeek8_Chybuk;
+
+public class CargoContainer<T> where T : DeliveryItem
+{
+    private List<T> _items = new List<T>();
+
+    public void AddItem(T item)
+    {
+        _items.Add(item);
+    }
+
+    public double GetTotalCost()
+    {
+        double total = 0;
+        foreach (var item in _items )
+            total += item.CalculateCost();
+        return total;
+    }
+}
